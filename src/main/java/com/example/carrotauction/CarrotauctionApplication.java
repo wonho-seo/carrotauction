45136c0671e062b4522882ab552bd57b71a1128a
sjdlr <sjdlrnen0507@naver.com>
2023-09-18 02:26:28 +0900

    Initial commit

---
+++ src/main/java/com/example/carrotauction/CarrotauctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrotauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotauctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carrotauction/CarrotauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrotauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
